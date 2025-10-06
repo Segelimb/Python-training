fc7bcd13b91ee6d31ec25a8fe67f2d073f2488ea
Segelimb <zalilova84@gmail.com>
2025-10-06 14:17:18 +0400

    add example 1

--- lesson3_zalilov.py
+++ lesson3_zalilov.py
@@ -1,1 +1,8 @@
 # Example 1
+# request = input('Надеюсь ты изучаешь python? ')
+# if request == 'Да, Python.':
+#     print('Молодец! Ты хороший!')
+#     print('C# хорошо, а Python лучше!')
+
+
+# Example 2
