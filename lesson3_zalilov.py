57ebc792885013f2c6da4871ae09f7a013df1d32
Segelimb <zalilova84@gmail.com>
2025-10-09 09:18:05 +0400

    add example 17

--- lesson3_zalilov.py
+++ lesson3_zalilov.py
@@ -153,3 +153,17 @@
 
 
 # Example 17
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# c = int(input("Введите третье число: "))
+
+# if a > 0 and b > 0 and c > 0:
+#     print(3)
+# elif (a > 0 and b > 0) or (a > 0 and c > 0) or (b > 0 and c > 0):
+#     print(2)
+# elif a > 0 or b > 0 or c > 0:
+#     print(1)
+# else: print(0)
+
+
+# Example 18
